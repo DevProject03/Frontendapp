11a40fd727409d7de8ff93c583034fd03cfd8c98
Teryisam <samueladeyongo@gmail.com>
2022-12-07 12:06:02 +0100

    Updated code with create and read

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+// https://23bf-80-248-14-174.eu.ngrok.io/api/SignUp/CreateNewAccount
 
 class CustmomerService {
   saveCustomer(customer) {
-    return axios.post(EMPLOYEE_API_BASE_URL, customer);
+    return axios.post("https://23bf-80-248-14-174.eu.ngrok.io/api/SignUp/CreateNewAccount");
   }
 
   getCustomers() {
